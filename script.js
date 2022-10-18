86fa92a2b7989cbc2e53b5bd30dc7213ba68a5f6
kaushikvj9254 <87133421+kaushikvj9254@users.noreply.github.com>
2022-10-18 17:02:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const display1El = document.querySelector(".display-1");
 const display2El = document.querySelector(".display-2");
-const tempResultEl = document.querySelector(".temp-result");
 const numbersEl = document.querySelectorAll(".number");
 const operationEl = document.querySelectorAll(".operation");
 const equalEl = document.querySelector(".equal");
@@ -21,7 +20,7 @@ numbersEl.forEach((number) => {
     }
     dis2Num += e.target.innerText;
     display2El.innerText = dis2Num;
-    // console.log();
+    
   });
 });
 
@@ -44,8 +43,8 @@ function clearVar(name = "") {
   dis1Num += dis2Num + " " + name + " ";
   display1El.innerText = dis1Num;
   display2El.innerText = "";
-  dis2Num = "";
-  tempResultEl.innerText = result;
+  dis2Num ="";
+ 
 }
 
 function mathOperation() {
@@ -61,7 +60,7 @@ function mathOperation() {
     result = parseFloat(result) % parseFloat(dis2Num);
   }
 }
-// operation();
+
 
 equalEl.addEventListener("click", () => {
   if (!dis2Num || !dis1Num) return;
@@ -69,7 +68,6 @@ equalEl.addEventListener("click", () => {
   mathOperation();
   clearVar();
   display2El.innerText = result;
-  tempResultEl.innerText = "";
   dis2Num = result;
   dis1Num = "";
 });
@@ -80,54 +78,13 @@ clearAllEl.addEventListener("click", () => {
   display1El.innerText = "";
   display2El.innerText = "";
   result = "";
-  tempResultEl.innerText = "";
 });
 
 clearLastEl.addEventListener("click", () => {
   display2El.innerText = "";
   dis2Num = "";
 });
-
-window.addEventListener("keydown", (e) => {
-  if (
-    e.key === "0" ||
-    e.key === "1" ||
-    e.key === "2" ||
-    e.key === "3" ||
-    e.key === "4" ||
-    e.key === "5" ||
-    e.key === "6" ||
-    e.key === "7" ||
-    e.key === "8" ||
-    e.key === "9" ||
-    e.key === "."
-  ) {
-    clickButtonEl(e.key);
-    // console.log(e.key)
-  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
-    clickOperation(e.key);
-  } else if (e.key === "*") {
-    clickOperation("x");
-    // console.log(e.key)
-  } else if (e.key == "Enter" || e.key === "=") {
-    clickEqual();
-  }
-  // console.log(e.key)
-});
-function clickButtonEl(key) {
-  numbersEl.forEach((button) => {
-    if (button.innerText === key) {
-      button.click();
-    }
-  });
-}
-function clickOperation(key) {
-  operationEl.forEach((operation) => {
-    if (operation.innerText === key) {
-      operation.click();
-    }
-  });
-}
+ 
 function clickEqual() {
   equalEl.click();
 }
